221c267e47d56e1da64089bc3b24de0d359ab829
NguyenHuuLuu <luuamazing@gmail.com>
2025-12-16 14:22:49 +0700

    AI and health service

--- backend/media-service/extensions.py
+++ backend/media-service/extensions.py
@@ -1,14 +1,4 @@
-from pymongo import MongoClient
-from config import app_config
+from flask_pymongo import PyMongo
 
-class MongoDB:
-    def __init__(self):
-        # Kết nối MongoDB bằng driver pymongo thuần
-        # Lấy URI từ biến môi trường hoặc file config
-        self.client = MongoClient(app_config.mongo_uri)
-        
-        # Tự động chọn database từ URI
-        self.db = self.client.get_database()
-
-# Khởi tạo instance
-mongo = MongoDB()
+# Khởi tạo MongoDB extension
+mongo = PyMongo()
