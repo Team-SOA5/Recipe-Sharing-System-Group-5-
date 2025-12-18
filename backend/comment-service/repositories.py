9f1fd76473d258aca2316b09b151bec3b766a618
someRandomGitHubAccount <76591563+someRandomGitHubAccount@users.noreply.github.com>
2025-12-18 16:51:06 +0700

    Add files via upload

--- backend/comment-service/repositories.py
+++ backend/comment-service/repositories.py
@@ -1,5 +1,35 @@
-from extensions import db
-from models import Comment
+from databases import db
+from models import *
+
+
+class UserRepository:
+    @staticmethod
+    def get_by_id(id: str) -> User:
+        return User.query.filter_by(user_id=id).first()
+
+    @staticmethod
+    def all() -> list[User]:
+        return User.query.all()
+
+    @staticmethod
+    def get_followers_of(user_id: str, page: int, limit_per_page: int) -> list[User]:
+        users: list[User] = (User.query.filter_by(user_id=user_id)
+                             .paginate(page=page, per_page=limit_per_page, error_out=False))
+
+        return users
+
+    @staticmethod
+    def get_users_following(user_id: str, page: int, limit_per_page: int) -> list[User]:
+        users: list[User] = (User.query.filter_by(user_id=user_id)
+                             .paginate(page=page, per_page=limit_per_page, error_out=False))
+
+        return users
+
+    @staticmethod
+    def save(user: User):
+        db.session.add(user)
+        db.session.commit()
+
 
 class CommentRepository:
     @staticmethod
@@ -7,8 +37,8 @@ class CommentRepository:
         return db.session.query(Comment).all()
 
     @staticmethod
-    def get_comment_by_id(comment_id: str) -> Comment:
-        return Comment.query.filter_by(id=comment_id).first()
+    def get_by_id(id: str) -> Comment:
+        return Comment.query.filter_by(id=id).first()
 
     @staticmethod
     def get_comments_to_recipe(recipe_id: str, page: int, limit_per_page: int,
@@ -28,11 +58,55 @@ class CommentRepository:
         return result
 
     @staticmethod
-    def save_comment(comment: Comment):
+    def save(comment: Comment):
         db.session.add(comment)
         db.session.commit()
 
     @staticmethod
-    def delete_comment(comment: Comment):
+    def delete(comment: Comment):
         db.session.delete(comment)
+        db.session.commit()
+
+
+class RatingRepository:
+    @staticmethod
+    def get_by_id(id: str) -> Rating:
+        return Rating.query.filter_by(id=id).first()
+
+    @staticmethod
+    def get_by_user_id(user_id: str) -> list[Rating]:
+        return Rating.query.filter_by(author=user_id).all()
+
+    @staticmethod
+    def get_by_author_id_and_recipe_id(author_id: str, recipe_id: str) -> Rating:
+        return Rating.query.filter_by(author=author_id, recipe_id=recipe_id).first()
+
+    @staticmethod
+    def get_all_of_recipe_id(recipe_id: str) -> list[Rating]:
+        return list(Rating.query.filter_by(recipe_id=recipe_id).all())
+
+    @staticmethod
+    def save(rating: Rating):
+        db.session.add(rating)
+        db.session.commit()
+
+    @staticmethod
+    def delete(rating: Rating):
+        db.session.delete(rating)
+        db.session.commit()
+
+
+class RecipeRepository:
+    @staticmethod
+    def get_by_id(id: str) -> Recipe:
+        return Recipe.query.filter_by(id=id).first()
+
+    @staticmethod
+    def get_liked_recipes_of(user_id: str, page: int, limit_per_page: int) -> list[Recipe]:
+        recipes: list[Recipe] = Recipe.query.paginate(page=page, per_page=limit_per_page, error_out=False)
+        return [r for r in recipes if r.is_favorited_by(user_id)]
+
+    @staticmethod
+    def save(recipe: Recipe):
+        db.session.add(recipe)
         db.session.commit()
